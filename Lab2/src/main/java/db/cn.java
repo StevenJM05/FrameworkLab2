d266283a9fa99ac11b7a98b09794ce6699c58d62
StevenJM05 <116135869+StevenJM05@users.noreply.github.com>
2024-09-23 08:16:49 -0600

    Modelos evento y lugar

---
+++ Lab2/src/main/java/db/cn.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author steve
+ */
+public class cn {
+    private Connection con;
+    
+    public cn() throws ClassNotFoundException{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch(Exception e){
+            System.err.println("Error" + e);
+        }
+    }
+    
+    public Connection getCon() { return con; }
+
+}
